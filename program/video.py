3a7de6c014de06728eff4ae3345948c702aad1fc
nitin268595 <90949794+nitin268595@users.noreply.github.com>
2021-11-23 14:15:39 +0530

    Update video.py

--- program/video.py
+++ program/video.py
@@ -169,7 +169,10 @@ async def vplay(c: Client, m: Message):
                 add_to_queue(chat_id, songname, dl, link, "Video", Q)
                 await loser.delete()
                 requester = f"[{m.from_user.first_name}](tg://user?id={m.from_user.id})"
-                await m.reply_text(f"💡 **Video Streaming Started in {chat_title} !**\n🏷 **Title:** [{songname}]({link})\n🎧 **By:** {requester}")
+                await m.reply_photo(
+                    photo=f"{IMG_2}",
+                    caption=f"💡 **Video Streaming Started in {chat_title} by {requester}**\n🏷 **Title:** [{songname}]({link})",
+                    )
         else:
             if len(m.command) < 2:
                 await m.reply(
