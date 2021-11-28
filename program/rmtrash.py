b851b057d862f6b82659cf74a746f5986088683e
nitin268595 <90949794+nitin268595@users.noreply.github.com>
2021-11-28 15:43:32 +0530

    Update rmtrash.py

--- program/rmtrash.py
+++ program/rmtrash.py
@@ -1,10 +1,16 @@
 # Copyright (C) 2021 By VeezMusicProject
 
 import os
+import re
+import uuid
+import socket
+import psutil
+import platform
+from config import BOT_USERNAME
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from driver.filters import command, other_filters
-from driver.decorators import sudo_users_only, errors
+from driver.decorators import sudo_users_only, errors, humanbytes
 
 downloads = os.path.realpath("program/downloads")
 raw = os.path.realpath(".")
@@ -48,3 +54,41 @@ async def cleanup(_, message: Message):
         await message.reply_text("✅ **cleaned**")
     else:
         await message.reply_text("✅ **already cleaned**")
+        
+@Client.on_message(command(["sysinfo", f"sysinfo@{BOT_USERNAME}"]) & ~filters.edited)
+@sudo_users_only
+async def give_sysinfo(client, message):
+    splatform = platform.system()
+    platform_release = platform.release()
+    platform_version = platform.version()
+    architecture = platform.machine()
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(socket.gethostname())
+    mac_address = ":".join(re.findall("..", "%012x" % uuid.getnode()))
+    processor = platform.processor()
+    ram = humanbytes(round(psutil.virtual_memory().total))
+    cpu_freq = psutil.cpu_freq().current
+    if cpu_freq >= 1000:
+        cpu_freq = f"{round(cpu_freq / 1000, 2)}GHz"
+    else:
+        cpu_freq = f"{round(cpu_freq, 2)}MHz"
+    du = psutil.disk_usage(client.workdir)
+    psutil.disk_io_counters()
+    disk = f"{humanbytes(du.used)} / {humanbytes(du.total)} " f"({du.percent}%)"
+    cpu_len = len(psutil.Process().cpu_affinity())
+    somsg = f"""🖥 **System Information**
+    
+**PlatForm :** `{splatform}`
+**PlatForm - Release :** `{platform_release}`
+**PlatFork - Version :** `{platform_version}`
+**Architecture :** `{architecture}`
+**Hostname :** `{hostname}`
+**IP :** `{ip_address}`
+**Mac :** `{mac_address}`
+**Processor :** `{processor}`
+**Ram : ** `{ram}`
+**CPU :** `{cpu_len}`
+**CPU FREQ :** `{cpu_freq}`
+**DISK :** `{disk}`
+    """
+    await message.reply(somsg)
