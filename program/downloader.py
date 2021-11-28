ee5711a8acb81f6a0c238fcf1ce00f8ef1fb1cae
nitin268595 <90949794+nitin268595@users.noreply.github.com>
2021-11-28 14:23:23 +0530

    Update downloader.py

--- program/downloader.py
+++ program/downloader.py
@@ -60,12 +60,13 @@ def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**🎧 Uploaded By: @{bn}**"
+        rep = f"**Uploaded By: {bn}**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
             secmul *= 60
         m.edit("📤 `Uploading...`")
+        m.edit("🗂 `Preparing File Name...`")
         message.reply_audio(
             audio_file,
             caption=rep,
