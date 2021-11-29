cec7dff35fda64ae1bff8812e6f8bac906a89e94
nitin268595 <90949794+nitin268595@users.noreply.github.com>
2021-11-29 11:02:03 +0530

    Update downloader.py

--- program/downloader.py
+++ program/downloader.py
@@ -60,7 +60,7 @@ def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**Uploaded By: {bn}**"
+        rep = f"`Uploaded By: {bn}`"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
@@ -261,7 +261,7 @@ async def vsong(client, message):
         file_name,
         duration=int(ytdl_data["duration"]),
         thumb=preview,
-        caption=ytdl_data["title"],
+        caption=ytdl_data`["title"]`,
     )
     try:
         os.remove(file_name)
