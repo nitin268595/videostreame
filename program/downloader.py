2e13340279c47f2d219a0506f010977a4aea833e
nitin268595 <90949794+nitin268595@users.noreply.github.com>
2021-12-01 16:22:26 +0530

    Update downloader.py

--- program/downloader.py
+++ program/downloader.py
@@ -249,7 +249,7 @@ async def vsong(client, message):
     except Exception as e:
         print(e)
     try:
-        msg = await message.reply("📥 `Downloading...`")
+        msg = await message.reply(progress=progress, progress_args=("📥 `Downloading...`"))
         with YoutubeDL(ydl_opts) as ytdl:
             ytdl_data = ytdl.extract_info(link, download=True)
             file_name = ytdl.prepare_filename(ytdl_data)
