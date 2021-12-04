17d6f2110824cd474ee84e8e5947480757523d84
nitin268595 <90949794+nitin268595@users.noreply.github.com>
2021-12-04 21:09:41 +0530

    Update downloader.py

--- program/downloader.py
+++ program/downloader.py
@@ -257,7 +257,7 @@ async def vsong(client, message):
         return await msg.edit(f"🚫 **Error:** {e}")
     preview = wget.download(thumbnail)
     start_time = time.time()
-    await msg.edit(progress=progress, progress_args=("**Trying to Upload Now!** \n, msg, start_time))
+    await msg.edit(progress=progress, progress_args=("**Trying to Upload Now!**, msg, start_time"))
     await message.reply_video(
         file_name,
         duration=int(ytdl_data["duration"]),
