1f0c28f9475ba91b44ed0d6beeb829f7be600198
nitin268595 <90949794+nitin268595@users.noreply.github.com>
2021-12-06 20:00:52 +0530

    Update video.py

--- program/video.py
+++ program/video.py
@@ -123,7 +123,7 @@ async def vplay(c: Client, m: Message):
         if replied.video or replied.document:
             loser = await replied.reply("Downloading")
             start_time = time.time()
-            await replied.download(progress=progress_bar, progress_args=("Downloading:",start_time, loser)
+            await replied.download(progress=progress_bar, progress_args=("Downloading:",start_time, loser))
             link = replied.link
             if len(m.command) < 2:
                 Q = 720
