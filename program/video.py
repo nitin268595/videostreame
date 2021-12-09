7139d891f4380b6ca4f2ffb5783caa62fcccfc1d
nitin268595 <90949794+nitin268595@users.noreply.github.com>
2021-12-09 16:06:59 +0530

    Update video.py

--- program/video.py
+++ program/video.py
@@ -5,7 +5,7 @@
 import asyncio
 import re
 import time
-from ..downloader import progress
+from program.downloader import progress
 from config import ASSISTANT_NAME, BOT_USERNAME, IMG_4, IMG_5
 from driver.decorators import sudo_users_only
 from driver.filters import command, other_filters
